23c15e6b8f35a0f41ad5c5458e0f12468b2aa1ae
Alvin Lee <alee0724@dhcp-vl2041-30299.eduroam.cornell.edu>
2024-02-20 01:32:38 -0500

    played around a bit, there is a weird % sign at the end of my prints for the numbers and I don't know why

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,3 @@
-open Printf
-
 (* Day 1 *)
 
 (* 
@@ -8,13 +6,23 @@ open Printf
 ascii chars 49-57 represent 1-9 numerically
 *)
 
-let rec get_digits (input:string) : string = 
-    try 
-        let num = int_of_string(String.sub input 0 1) in 
-        string_of_int num ^ get_digits(String.sub input 1 ((String.length input) - 1))
-    with
-        | _ -> get_digits (String.sub input 1 (String.length input - 1))
+let rec get_digits (input:string) : int list = 
+    let hd = String.sub input 0 1 in 
+    try
+        let irep = int_of_string hd in 
+        match String.length input with 
+        | 1 -> [irep] 
+        | _ -> irep :: get_digits (String.sub input 1 ((String.length input) - 1))
+    with 
+    | _ -> 
+            match String.length input with 
+            | 1 -> []
+            | _ -> get_digits (String.sub input 1 ((String.length input) - 1))
+
+let get_2digit (input:string) : int = 
+    let digits = get_digits input in
+    (List.nth digits 0)*10 + List.nth digits (List.length digits-1)
 
 let () = 
     let input = read_line() in
-    printf "%s" (get_digits input)
+    print_int (get_2digit input);
